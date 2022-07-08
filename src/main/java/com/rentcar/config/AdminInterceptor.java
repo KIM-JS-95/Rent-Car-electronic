babc56ff5c45896ddb3db88475583cd31eb3453a
yuseons <yusnseo@gmail.com>
2022-07-08 16:33:22 +0900

    0708

--- src/main/java/com/rentcar/config/AdminInterceptor.java
+++ src/main/java/com/rentcar/config/AdminInterceptor.java
@@ -1,5 +1,5 @@
 package com.rentcar.config;
- 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.rentcar.utility.Utility;
- 
+
 public class AdminInterceptor extends HandlerInterceptorAdapter {
-// URL 요청후 컨트롤러 작동전에 실행함.
+  // URL 요청후 컨트롤러 작동전에 실행함.
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     HttpSession session = request.getSession(true);
@@ -26,20 +26,19 @@ public class AdminInterceptor extends HandlerInterceptorAdapter {
       return false;
     }
   }
- 
-// 요청 주소 결과 생성후 작동함
+
+  // 요청 주소 결과 생성후 작동함
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-      ModelAndView modelAndView) throws Exception {
+                         ModelAndView modelAndView) throws Exception {
     System.out.println("postHandle executed.");
     modelAndView.addObject("admin", "관리자 관련 기능을 출력합니다.");
   }
- 
-// JSP등 View 페이지 출력전에 작동됨.
+
+  // JSP등 View 페이지 출력전에 작동됨.
   @Override
   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-      throws Exception {
+          throws Exception {
     System.out.println("관리자 Interceptor 실행 후 최종 출력됨.");
   }
 }
- 
---
+++ src/main/java/com/rentcar/config/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.rentcar.config;
+
+import com.rentcar.list.UploadList;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    registry.addResourceHandler("/ckstorage/files/**")
+            .addResourceLocations("file:///"+ UploadList.getUploadDir()+"/files/");
+  }
+
+}
