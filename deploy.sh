02ac25db0857e4293974539e41857c67abf76fab
KIM-JS-95 <baugh248730@gmail.com>
2022-07-27 22:58:56 +0900

    123

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,15 @@
 container_name="isosim";
 repository="baugh248730/isosim";
 
+echo "docker logins try"
+docker login
+
 # 기존 컨테이너 정지 후 이미지 제거
 echo "Container stop and remove";
 
 # shellcheck disable=SC1007
 container_id=$(docker ps -aqf "name= ${container_name}");
-echo "docker logins try"
-docker login
+
 echo "Docker : container_id = ${container_id}";
 
 echo "Container stop"
@@ -40,6 +42,6 @@ echo "Docker compose up start ... "
 docker-compose up -d
 
 
-#echo "image push to docker hub"
-#docker tag isosim baugh248730/isosim:latest
-#docker push ${repository}
+echo "image push to docker hub"
+docker tag isosim baugh248730/isosim:latest
+docker push ${repository}
